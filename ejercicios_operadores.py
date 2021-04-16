36d58a80d27011015096c53dfc4286eb06d775b5
Pilarpp <gudelia.perez353@uppuebla.edu.mx>
2021-04-15 23:55:07 -0500

    Create ejercicios_operadores.py

---
+++ ejercicios_operadores.py
@@ -0,0 +1,44 @@
+####Operator (power **) exponent
+##print(2**3)
+##print(2 ** 3)
+##print(2 ** 3.)
+##print(2. ** 3)
+##print(2. ** 3.)
+
+
+##Operator (multiplication *)
+##print(2*3)
+##print(2 * 3)
+##print(2 * 3.)
+##print(2. * 3)
+##print(2. * 3.)
+
+##Operator (division /)
+##print(2/3)
+##print(2 / 3)
+##print(2 / 3.)
+##print(2. / 3)
+##print(2. / 3.)
+
+##Operator (integer divisional //)
+##print(-6//4)
+##print(7 // 3)
+##print(6 // 3.)
+##print(6. // -4)
+##print(7. // 3.)
+
+##Operator (modulo %)
+##print(5%2)
+##print(12%4.5)
+
+##Operator (plus +)
+##print(5+2)
+##print(12+ 4.5)
+
+##Operator (munis -)
+##print(-4 -4 )
+##print(4. -8)
+##print(-1.1)
+
+##Hierarchy of priorities
+print(2+3*5**2/2)
